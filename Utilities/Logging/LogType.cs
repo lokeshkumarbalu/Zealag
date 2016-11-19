4b409af07dd0ac3573dc3184e29fab3c6e57ed90
lbalu <lbalu@BLRLBALU45390>
2016-11-19 12:51:50 +0100

    Add LogType.cs

---
+++ Utilities/Logging/LogType.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LogType.cs" company="Zealag">
+//    Copyright © Zealag 2016. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Zealag.Utilities.Logging
+{
+    /// <summary>
+    /// Specifies constants that define Log types that are available from this namespace.
+    /// </summary>
+    public enum LogType : byte
+    {
+        /// <summary>
+        /// The Console log.
+        /// </summary>
+        ConsoleLog = 1,
+
+        /// <summary>
+        /// The File log.
+        /// </summary>
+        FileLog
+    }
+}
