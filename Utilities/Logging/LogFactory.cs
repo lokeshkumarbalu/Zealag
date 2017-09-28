c7ffecf17abc2709060a550ec41edc35c6ab2af3
Lokesh Kumar Balu <lokeshkumarbalu@gmail.com>
2017-09-29 00:23:56 +0200

    Change name to identify more easily the factory.

---
+++ Utilities/Logging/LogFactory.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LogFactory.cs" company="Zealag">
+//     Copyright © Zealag 2016. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Zealag.Utilities.Logging
+{
+    using System;
+
+    /// <summary>
+    /// The static factory class to create Log objects when provided with the enum value from <see cref="LogType"/>.
+    /// </summary>
+    public static class LogFactory
+    {
+        /// <summary>
+        /// The static method to create <see cref="Log"/> objects for logging purpose.
+        /// </summary>
+        /// <returns>Should return an instance of <see cref="Log"/>.</returns>
+        /// <exception cref=""
+        public static Log GetLog(LogType logType)
+        {
+            string logClassName = "";
+            object returnValue = null;
+            Type logClassType = null;
+            
+            logClassName = Enum.GetName(typeof(LogType), logType);            
+            logClassType = Type.GetType(typeof(LogFactory).Namespace + "." + logClassName);
+            returnValue = Activator.CreateInstance(logClassType, true);
+
+            return (Log)returnValue;
+        }
+    }
+}
